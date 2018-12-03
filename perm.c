fd042e2ac8d6492d7957f8a90db75e9b7a3dfc3a
dcc0 <ivangavr777@gmail.com>
2018-12-03 20:31:13 +0300

    Create perm.c

---
+++ perm.c
@@ -0,0 +1,16 @@
+//Числа девятке кратные
+#include <stdio.h>
+
+int main() {
+
+	int a =1234567;
+	int b = 7654321-1234567;
+	int c = a;
+	int d = 10;
+	int i;
+    for (i=0; i!=b; i+=9) {
+		 c=a+i;
+			if (a-c==0)
+		 printf("%d\n", c);
+	}
+}
