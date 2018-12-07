f00afd39629aaae558b8bd616ec546c65bac1ac3
dcc0 <ivangavr777@gmail.com>
2018-12-07 23:57:24 +0300

    Create permutations_spiral.php

---
+++ permutations_spiral.php
@@ -0,0 +1,24 @@
+<?php
+$n = 7;
+$fact = 5040;
+$perm = "1234567";
+for ($i = 1; $i != $fact + 1; $i++)
+	{
+	print $perm . "\n";
+	$mm = $i;
+	$m = $n;
+	while ($m > 0)
+		{
+		if ($mm % $m == 0) {
+			$mm/= $m--;
+			}
+		  else
+			{
+			$perm = substr($perm, $n - $m + 1).strrev(substr($perm, 0, $n - $m + 1));
+			continue 2;
+			}
+		}
+	}
+
+
+?>
