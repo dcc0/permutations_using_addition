1189d294b1400aaaf10d68fb27fbf95b87119b64
dcc0 <ivangavr777@gmail.com>
2018-12-09 19:47:50 +0300

    Update cycled_permutations_for_superpermutations.php

--- cycled_permutations_for_superpermutations.php
+++ cycled_permutations_for_superpermutations.php
@@ -1,42 +1,36 @@
 <?php
-$n=4;
-$fact=24;
-$perm="1234";
-$seq="123";
+$n = 4;
+$fact = 24;
+$perm = "1234";
+$seq = "123";
 
-
-    for ($i=1; $i != $fact+1; $i++) {
-
-			print $perm . "\n";
-			$seq.=$perm[$n-1];
-
-			$mm = $i;
-			$m=$n;
-
-			while ($m > 0) {
-
-			if ($mm%$m==0)  {
-				$mm /= $m--;
-				print ("-----\n");
-				$seq.='.';
-			}
-			else
+for ($i = 1; $i != $fact + 1; $i++)
+	{
+	print $perm . "\n";
+	$seq.= $perm[$n - 1];
+	$mm = $i;
+	$m = $n;
+	while ($m > 0)
+		{
+		if ($mm % $m == 0)
 			{
-
-			$tail= strrev(substr($perm, 0, $n-$m+1));
-			$head = substr($perm,  $n-$m+1);
-			$perm=$head.$tail;
-
-			continue 2;
-				}
-
+			$mm/= $m--;
+			print ("-----\n");
+			$seq.= '.';
 			}
+		  else
+			{
+			$tail = strrev(substr($perm, 0, $n - $m + 1));
+			$head = substr($perm, $n - $m + 1);
+			$perm = $head . $tail;
+			continue 2;
+			}
+		}
 
-				$sw=$perm[0];
-			    for ($j=0; $j<$n-1; $j++) $perm[$j]=$perm[$j+1];
-				$perm[$n-1]=$sw;
+	$sw = $perm[0];
+	for ($j = 0; $j < $n - 1; $j++) $perm[$j] = $perm[$j + 1];
+	$perm[$n - 1] = $sw;
+	}
 
-
-}
 print "\n" . $seq;
 ?>
