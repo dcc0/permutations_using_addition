17c98a0b878354ac4de1767587f82bc2122ef244
dcc0 <ivangavr777@gmail.com>
2018-12-16 00:50:31 +0300

    Update random_permutations_ok.php

--- random_permutations_ok.php
+++ random_permutations_ok.php
@@ -4,28 +4,27 @@
 			$arr=array();
 			$i=0;
 			$value=null;
-		
-		
+
+
 			while (count($arr)!=$fact) {
-		
+
 				for ($j=0; $j !=$n; $j++) {
 				$var=rand(1, $n);
 				$value.=$var;
 				$temp[]=$var;
 				}
-		      /*Поиск уникальных. Условие добавления - отсутствие ключа в массиве*/
-				if (count(array_unique($temp))==$n && !array_key_exists($value, $arr))
+
+				if (count(array_unique($temp))==$n && !isset($arr[$value]))
 				$arr[$value]=$i++;
 				//print ($value . "\n");
-		
+
 			unset($temp);
 			$var="";
 			$value="";
-		
+
 			}
-		/*Печатаем ключи массива, они и есть перестановки*/
+
 			foreach ($arr as $k => $v)
 			print ($k . "\n");
-		
+
 ?>
-		
