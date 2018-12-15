623e9997a4f76a174b0e5d10d1e07a5c0f9e2392
dcc0 <ivangavr777@gmail.com>
2018-12-15 20:29:55 +0300

    Update random_unique_permutations.php

--- random_unique_permutations.php
+++ random_unique_permutations.php
@@ -1,22 +1,24 @@
 <?php
-	$n=7;
-	$fact=5040;
+	$n=8;
+	$fact=40320;
 	$arr=array();
 	$i=1;
 
 	while (++$i) {
 		for ($j=0; $j !=$n; $j++) {
-		$temp[]=rand(1, $n);
-		$var.=rand(1, $n);
+		$var=rand(1, $n);
+		$value.=$var;
+		$temp[]=$var;
 		}
-		if (count(array_unique($temp))==$n && !in_array($var, $arr)) {
-		$arr[]=$var;
-		print ($var . "\n");
+		if (count(array_unique($temp))==$n && !in_array($value, $arr)) {
+		$arr[]=$value;
+		print ($value . "\n");
 		}
 
 		if (count($arr)==$fact) break;
 	unset($temp);
 	$var="";
+	$value="";
 	}
 
 	print_r(array_unique($arr));
