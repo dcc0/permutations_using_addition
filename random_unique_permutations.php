3f44031a125e39a5f6ada5f04bf279e52b217cbf
dcc0 <ivangavr777@gmail.com>
2018-12-15 19:38:14 +0300

    Create random_unique_permutations.php

---
+++ random_unique_permutations.php
@@ -0,0 +1,19 @@
+<?php
+	$n=4;
+	$fact=24;
+	$arr=array();
+	$i=1;
+
+	while (++$i) {
+		for ($j=0; $j !=$n; $j++)
+		$temp[]=rand(1, $n);
+
+		if (count(array_unique($temp))==$n)
+		$arr[]=implode('', $temp);
+
+		if (count(array_unique($arr))==$fact) break;
+	unset($temp);
+	}
+
+	print_r(array_unique($arr));
+?>
